bc2d1263d192c46925c0fa60c57980958b6c6d8f
jinmu333 <dreamhunter2333@gmail.com>
2022-03-30 00:09:38 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,40 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'se') {
+        try {
+            const res = await axios.get(process.env.URL);
+            const url = res.data;
+            console.log('Get url: ' + url);
+            await interaction.reply(url);
+        } catch (error) {
+            await interaction.reply('请稍后重试');
+        }
+
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
