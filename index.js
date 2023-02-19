5454b5855345309e6ef4338e39f2b3320206f854
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-02-19 14:48:57 +0800

    feat: fix GUILD_MESSAGES

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const axios = require('axios');
 dotenv.config();
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ],
+});
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
