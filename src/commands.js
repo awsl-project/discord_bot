bedb526cdd9c363a852ffa2a7e2df73f3aad6d56
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-03-06 13:53:36 +0800

    migrate to cf

---
+++ src/commands.js
@@ -0,0 +1,19 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const SE_COMMAND = {
+  name: 'se',
+  description: 'Awsl 随机一图',
+};
+
+export const MO_COMMAND = {
+  name: 'mo',
+  description: '摸鱼办',
+};
+
+export const AI_TEXT_COMMAND = {
+  name: 'ai_text',
+  description: 'AI 文本',
+};
